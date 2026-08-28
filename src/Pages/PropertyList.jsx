5fb288835c9674092615e37124a159429eb67804
akki172 <kattamuriakshaya@gmail.com>
2026-08-28 14:53:47 +0530

    Fix PropertyList CSS import case

--- src/Pages/PropertyList.jsx
+++ src/Pages/PropertyList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "../css/PropertyList.css";
+import "../css/Propertylist.css";
 import AIAssistant from "../components/AIAssistant";
 
 
